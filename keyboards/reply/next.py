39d2d16efdefb27f5dda0efcfc481a7805582329
aisulu <zharasovna.24.01@gmail.com>
2023-12-10 20:43:41 +0500

    replykeyboard resized

--- keyboards/reply/next.py
+++ keyboards/reply/next.py
@@ -1,5 +1,5 @@
 from telebot import types
 
 
-next_reply = types.ReplyKeyboardMarkup()
+next_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
 next_reply.row(types.KeyboardButton('Продолжить'))
